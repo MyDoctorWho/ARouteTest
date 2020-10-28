0c1c952dd799b25249001f80fa5724b6131f4cec
sunhewei <sunhewei@mobimagic.com>
2020-10-28 20:34:52 +0800

    first commit

---
+++ BaseModule/src/main/java/com/weidi/basemodule/MainActivity.java
@@ -0,0 +1,16 @@
+package com.weidi.basemodule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ ModuleA/src/main/java/com/weidi/modulea/ModuleAMainActivity.java
@@ -0,0 +1,17 @@
+package com.weidi.modulea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+@Route(path = "/path/moduleA")
+public class ModuleAMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_module_amain);
+    }
+}
